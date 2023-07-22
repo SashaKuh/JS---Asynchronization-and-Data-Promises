cfc26c39b291e2d4028d77aec6c8afdc31e7a260
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-07-22 21:56:01 +0300

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,34 +7,23 @@ form.addEventListener('submit', onSubmit);
 
 function onSubmit(evt) {
   evt.preventDefault();
-
-  const { delay, step, amount } = {
-    delay: Number(evt.currentTarget.delay.value),
-    step: Number(evt.currentTarget.step.value),
-    amount: Number(evt.currentTarget.amount.value),
-  };
-
-  let i = 0;
-
-  function sendNotification() {
-    if (i < amount) {
-      let promiseDelay = delay + step * i;
-      createPromise(i + 1, promiseDelay)
-        .then(({ position, delay }) => {
-          Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-        })
-        .catch(({ position, delay }) => {
-          Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-        })
-        .finally(() => {
-          i++;
-          sendNotification();
-        });
-    }
+  
+  let delay = Number(evt.currentTarget.delay.value)
+  const step= Number(evt.currentTarget.step.value)
+  const amount= Number(evt.currentTarget.amount.value)
+  
+  for (let i = 1; i <= amount; i += 1) {
+  createPromise(i, delay)
+  .then(({ position, delay }) => {
+  Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+  Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+  })
+  
+  delay += step
   }
-
-  sendNotification();
-}
+  }
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {

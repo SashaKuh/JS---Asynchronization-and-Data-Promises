844d95ae8b129919db3255dabc5586e3c07f6054
SashaKuh <71312391+SashaKuh@users.noreply.github.com>
2023-07-22 17:59:44 +0300

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,34 +8,44 @@ form.addEventListener('submit', onSubmit);
 function onSubmit(evt) {
   evt.preventDefault();
 
-  const {delay, step, amount} = {
+  const { delay, step, amount } = {
     delay: Number(evt.currentTarget.delay.value),
     step: Number(evt.currentTarget.step.value),
-    amount: Number(evt.currentTarget.amount.value)
+    amount: Number(evt.currentTarget.amount.value),
+  };
+
+  let i = 0;
+
+  function sendNotification() {
+    if (i < amount) {
+      let promiseDelay = delay + step * i;
+      createPromise(i + 1, promiseDelay)
+        .then(({ position, delay }) => {
+          Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        })
+        .finally(() => {
+          i++;
+          sendNotification();
+        });
+    }
   }
 
-  for (let i = 0; i < amount; i++) {
-    let promiseDelay = delay + step * i;
-    createPromise(i++, promiseDelay)
-      .then(({ position, delay }) => {
-        Notify.success(`Fulfilled promise ${position} in ${delay}ms`)
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`Rejected promise ${position} in ${delay}ms`)
-      });;
-  }
-};
+  sendNotification();
+}
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       const shouldResolve = Math.random() > 0.3;
       if (shouldResolve) {
-        resolve({ position, delay })
+        resolve({ position, delay });
       } else {
-        reject({ position, delay })
+        reject({ position, delay });
       }
-    }, delay)
-  })
+    }, delay);
+  });
   return promise;
-};
+}
